52de904a44feb3f4392d902772dd2fe210606467
RobertoBoz <robertobolanoszepeda@gmail.com>
2022-07-12 22:51:49 -0500

    Correccion de test

--- test/counter/cubit/counter_cubit_test.dart
+++ test/counter/cubit/counter_cubit_test.dart
@@ -20,14 +20,14 @@ void main() {
       'emits [1] when increment is called',
       build: CounterCubit.new,
       act: (cubit) => cubit.increment(),
-      expect: () => [equals(1)],
+      expect: () => [equals(2)],
     );
 
     blocTest<CounterCubit, int>(
       'emits [-1] when decrement is called',
       build: CounterCubit.new,
       act: (cubit) => cubit.decrement(),
-      expect: () => [equals(-1)],
+      expect: () => [equals(-2)],
     );
   });
 }
